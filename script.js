e746b0028d769b53e9949aa230624a5411fcee7c
Caio M. <cmacedo20012006@gmail.com>
2026-04-20 19:04:18 -0300

    Conteúdo em script.js
    Adicionei as perguntas no arquivo script.js , assim como suas pontuações e textos.

---
+++ script.js
@@ -0,0 +1,73 @@
+const formulario =() => {
+    {pergunta:"Qual é o seu maior objetivo?"
+        resposta:[
+            {id:1, texto: "Ser reconhecido e admirado por todos",naruto_pontos: 3,sasuke_pontos: 1, sakura_pontos: 2},
+            {id:2, texto: "Se tornar mais forte a qualquer custo",naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 2},
+            {id:3, texto: "Ajudar a proteger as pessoas que amo",naruto_pontos: 2,sasuke_pontos: 1, sakura_pontos: 3}
+        ]
+    }
+        {pergunta:"Como você lida com desafios?"
+        resposta:[
+            {id:1, texto: "Nunca desisto,mesmo se for difícil",naruto_pontos: 3,sasuke_pontos: 2, sakura_pontos: 1},
+            {id:2, texto: "Analiso friamente e faço o necessário para vencer",naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 2},
+            {id:3, texto: "Busco equilíbrio e estratégiacom cautela",naruto_pontos: 2,sasuke_pontos: 1, sakura_pontos: 3}
+        ]
+    }
+        {pergunta:"Em um trabalho em equipe, você:"
+        resposta:[
+            {id:1, texto: "Motiva todos e mantém o espírito alto",naruto_pontos: 3,sasuke_pontos: 1, sakura_pontos: 2},
+            {id:2, texto: "Prefere agir sozinho",naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 1},
+            {id:3, texto: "Coopera e apoia seus colegas",naruto_pontos: 2,sasuke_pontos: 1, sakura_pontos: 3}
+        ]
+    }
+        {pergunta:"Como você reage quando está com raiva?"
+        resposta:[
+            {id:1, texto: "Explodo, mas logo volto ao normal",naruto_pontos: 3,sasuke_pontos: 2, sakura_pontos: 1},
+            {id:2, texto: "Guardo e uso isso como motivação",naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 2},
+            {id:3, texto: "Tento me controlar e resolver com calma",naruto_pontos: 2,sasuke_pontos: 1, sakura_pontos: 3}
+        ]
+    }
+       {pergunta:"Qual dessas qualidades mais combina com você?"
+        resposta:[
+            {id:1, texto: "Determinação",naruto_pontos: 3,sasuke_pontos: 2, sakura_pontos: 1},
+            {id:2, texto: "Ambição",naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 2},
+            {id:3, texto: "Inteligência",naruto_pontos: 1,sasuke_pontos: 2, sakura_pontos: 3}
+        ]
+    }
+       {pergunta:"O que é mais importante pra você?"
+        resposta:[
+            {id:1, texto: "Amizade",naruto_pontos: 3,sasuke_pontos: 1, sakura_pontos: 2},
+            {id:2, texto: "Poder",naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 1},
+            {id:3, texto: "Crescimento pessoas",naruto_pontos: 2,sasuke_pontos: 1, sakura_pontos: 3}
+        ]
+    }
+       {pergunta:"Como você lida com perdas?"
+        resposta:[
+            {id:1, texto: "Sigo em frente com força e esperança",naruto_pontos: 3,sasuke_pontos: 2, sakura_pontos: 1},
+            {id:2, texto: "Isso me muda profundamente", naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 2},
+            {id:3, texto: "Busco aprender e evoluir com a experiência",naruto_pontos: 2,sasuke_pontos: 1, sakura_pontos: 3}
+        ]
+    }
+       {pergunta:"Qual é o seu estilo de aprendizado?"
+        resposta:[
+            {id:1, texto: "Aprendo sozinho",naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 1},
+            {id:2, texto: "Aprendo com certa ajuda dos outros",naruto_pontos: 1,sasuke_pontos: 1, sakura_pontos: 3},
+            {id:3, texto: "Aprendo com ajuda excessiva dos outros",naruto_pontos: 3,sasuke_pontos: 1, sakura_pontos: 1}
+        ]
+    }
+        {pergunta:"Como você prefere evoluir?"
+        resposta:[
+            {id:1, texto: "Treinando duro e persistindo",naruto_pontos: 3,sasuke_pontos: 2, sakura_pontos: 1},
+            {id:2, texto: "Superando limites rapidamente",naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 2},
+            {id:3, texto: "Estudando e aperfeiçoando técnicas",naruto_pontos: 2,sasuke_pontos: 1, sakura_pontos: 3}
+        ]
+    }
+        {pergunta:"Como você quer ser lembrado?"
+        resposta:[
+            {id:1, texto: "Como alguém que nunca desistiu",naruto_pontos: 3,sasuke_pontos: 2, sakura_pontos: 1},
+            {id:2, texto: "Como alguém extremamente poderoso",naruto_pontos: 1,sasuke_pontos: 3, sakura_pontos: 2},
+            {id:3, texto: "Como alguém útil e confiável",naruto_pontos: 2,sasuke_pontos: 1, sakura_pontos: 3}
+        ]
+    }
+};
+
